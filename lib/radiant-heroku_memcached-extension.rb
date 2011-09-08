902cce1344e5eabe84a3b398c1cc36161ac0bed8
jsntv200 <jsntv200@gmail.com>
2011-09-08 12:30:39 +1000

    bump version

--- lib/radiant-heroku_memcached-extension.rb
+++ lib/radiant-heroku_memcached-extension.rb
@@ -1,5 +1,5 @@
 module RadiantHerokuMemcachedExtension
-  VERSION     = "1.0.0"
+  VERSION     = "1.0.1"
   SUMMARY     = "Heroku Memcached for Radiant CMS"
   DESCRIPTION = "Use memcached with Radiant running on Heroku"
   URL         = "https://github.com/jsntv200/radiant-heroku_memcached-extension"
